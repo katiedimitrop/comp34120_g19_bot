3b405ccaff46794815285245fd351c16e481de3e
katdm <catherine.dimitropoulaki@student.manchester.ac.uk>
2019-11-12 13:43:14 +0000

    Changed the function parse State Change
    After printing the board within make move we realized that the 0th column of
    the board did not store the scores as intended. I made changes to parseState
    Change in message.py to fix this. However now the agent aborts after playing
    one move as south.

--- g19Bot/game.py
+++ g19Bot/game.py
@@ -10,8 +10,25 @@ def makeMove():
     pitIndex = 1
     bestPit = 1
     f.write("Searching for best move \n")
+
+   #DEBUG: score pits
+    scoreNorth = board.getSeeds(0,0)
+    scoreSouth = board.getSeeds(1,0)
+    f.write("Pit no:"+str(0)+"\n")
+    f.write("North score:"+ str(scoreNorth)+"\n")
+    f.write("South score:"+str(scoreSouth)+"\n")
+
+
+    #DEBUG: for each pit print out the number of seed in it
+    for pit in range(1,8):
+        seedNorth = board.getSeeds(0,pit)
+        seedSouth = board.getSeeds(1,pit)
+        f.write("Pit no:"+str(pit)+"\n")
+        f.write("North seeds:"+ str(seedNorth)+"\n")
+        f.write("South seeds:"+str( seedSouth)+"\n")
+
     #Strategy: find next pit where pitNum > 0
-    while (pitIndex <7):
+    while (pitIndex <=7):
         seedNum = board.getSeeds(board.agentSide,pitIndex)
         # If current pit is empty, move on to next pit
         if (seedNum == 0):
@@ -21,7 +38,7 @@ def makeMove():
           #f.write("Current seed number "+str(seedNum)+"\n")
           #f.write("Current best pit "+str(bestPit)+"\n")
         # Move that makes final seed land in score pit giving extra move (best)
-        elif (seedNum + pitIndex == 7):
+        elif (seedNum + pitIndex == 8):
           bestPit = pitIndex
           #f.write("BEST MOVE \n")
           #f.write("Current pit index "+str(pitIndex)+"\n")
@@ -29,16 +46,14 @@ def makeMove():
           #f.write("Current best pit "+str(bestPit)+"\n")
           break
         # Move that gets a seed in final pit and some in opponents pit (second best)
-        elif (seedNum + pitIndex >= 7):
+        elif (seedNum + pitIndex >= 8):
           bestPit = pitIndex
           pitIndex+=1
           #f.write("SECOND BEST \n")
           #f.write("Current pit index "+str(pitIndex)+"\n")
           #f.write("Current seed number "+str(seedNum)+"\n")
           #f.write("Current best pit "+str(bestPit)+"\n")
-        
-
-        # Any other viable move
+           # Any other viable move
         else:
           bestPit = pitIndex
           pitIndex+=1
@@ -49,11 +64,9 @@ def makeMove():
         #debug statements
         #f.write("Current pit index "+str(pitIndex)+"\n")
         #f.write("Current seed number "+str(seedNum)+"\n")
-        
-    f.write("MOVE;"+str(bestPit+1)+"\n")
 
-
-    msg.moveMsg(bestPit+1)
+    f.write("MOVE;"+str(bestPit)+"\n")
+    msg.moveMsg(bestPit)
 
 
 def changeProtocol(line):
@@ -98,7 +111,7 @@ def run_game():
       if (msgType == "END"):
         break
       messageAction(line, msgType)
-        #board.toString()      #uncomment to debug
+      #board.toString()      #uncomment to debug
   except:
     sys.stderr.write("Something went wrong", sys.exc_info()[0])
 
--- g19Bot/message.py
+++ g19Bot/message.py
@@ -1,6 +1,6 @@
 import sys
 
-def getMsgType(line):  
+def getMsgType(line):
     if (line == "END\n"):
         return "END"
     words = line.split(";")
@@ -26,23 +26,23 @@ def isPlayerNorth(line):
 def parseStateChange(line, holes):
     words = line.split(";")
     state = words[2].split(",")
-    pits = holes + 1 
-    boardArray = [[0] * (pits) for i in range(2)] #initialise empty 2D array
-    for i in range(0, (pits)):
-        boardArray[0][i] = int(state[i])
-        boardArray[1][i] = int(state[i + pits])
+    pits = holes #excluding score pit
+    boardArray = [[0] * (pits+1) for i in range(2)] #initialise empty 2D array
+    #for all pits except score pit
+    for stateIndex in range(0, pits): #0 to 7
+        boardArray[0][stateIndex +1] = int(state[stateIndex])
+        boardArray[1][stateIndex +1] = int(state[stateIndex + pits])
+
+    boardArray[0][0] = int(state[pits+1])
+    boardArray[1][0] = int(state[2*(pits+1)])
     return boardArray
 
 def isSwap(line):
     words = line.split(";")
     if(words[1] == "SWAP"):
-        return True 
+        return True
     return False
 
 def getTurn(line):
     words = line.split(";")
     return(words[3])
-
-
-
-
