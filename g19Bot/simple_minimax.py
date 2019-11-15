fb1bdd2ea8c2a1dadab32f1ad7aa219394b6dbe2
katdm <catherine.dimitropoulaki@student.manchester.ac.uk>
2019-11-15 13:09:45 +0000

    rewriting and debugging for depth 1 game

--- g19Bot/simple_minimax.py
+++ g19Bot/simple_minimax.py
@@ -1,8 +1,7 @@
-# A simple Python3 program that returns the index of a move our agent should make
-#(range: [1,7])
-#MM is a backtracking based algorithm, for each possible current move it looks
-#ahead a certain number of turns (treeDepth) then backtracks and makes a well informed
-#move. (DFS)
+#Will return the index of a move our agent should make (range: [1,7])
+#MiniMax is a backtracking based algorithm, for each possible current move it
+#looks ahead (DFS) a certain number of turns (treeDepth) then backtracks and makes a
+#well informed move.
 
 #an alternative would be to create one big tree that is used to inform multiple
 #consecutive moves. But a big tree like that  would require storing.
@@ -13,96 +12,123 @@ import math
 from sys import maxsize
 import numpy as np
 
-def minimax (curDepth, nodeIndex,
-			maxTurn, scores,
-			targetDepth,branchFactor,currentBoardState, moveIndex):
+def minimax (curDepth, nodeIndex, isMaxTurn, scores, leafDepth, branchFactor
+			, currentBoard, moveIndex):
     #DEBUG:this represents the depth
 	print("D"+str(curDepth)+": ")
 
-	# base case : targetDepth (max depth) reached
-	if (curDepth == targetDepth):
+	# base case : leafDepth (max depth) reached
+	if (curDepth == leafDepth):
 		#DEBUG:Leaf index and value
-		#print("LEAF node no."+str(nodeIndex) +" value: "+ str(scores[nodeIndex]))
-        #use the state that has been passed down to compute value
 
+		#Leaves are the only node in which the board passed to them is evaluated
+		#and the score returned up
 
-		print("Board state after move: "+str(moveIndex + 1) + ":")
-		#Board changer, takes a Board.board 2d array, a move index
-		#and returns the next board.
-		nextB = nextBoard(maxTurn, currentBoardState, moveIndex)
-		print(str(nextB))
-		score = evaluateBoard(nextB)
+		#DEBUG
+		print("EVALUATING:"+str(currentBoard))
+		score = evaluateBoard(currentBoard)
+
+		#save valuation
 		scores[nodeIndex] = score
-		#BoardEvaluator
-		return scores[nodeIndex]
 
-	if (maxTurn):
-		#for storing the child valuations
+		#return to parents
+		return score
+
+	if (isMaxTurn):
+		#print node description 
+		playerName = "SOUTH (MAX)"
+
+		print(playerName+ " node "+ "with state:")
+		print(currentBoard)
+		print('\n')
+
+		#Child scores will be sent up and stored here
 		moves = [0]*branchFactor
 
-		print("Boardstate passed to leaves:")
-		print(currentBoardState)
-		print('\n')
-		#Run minimax on each child (future turns)
+		#Run minimax on each child (next turns)
 		for moveIndex in range (0,branchFactor):
-			print("Start MM")
+			print("If "+ playerName + " MOVES "+str(moveIndex+1))
+			print(playerName + " CHILD NO."+str(moveIndex+1)+" MM")
+			print("Its state will be: ")
+
+			#Get board produced by this move from this node
+			nextBoard = makeNextBoard(isMaxTurn, currentBoard, moveIndex)
+			print(str(nextBoard))
+
+			#pass board to child
 			moves[moveIndex] = minimax(curDepth + 1,
-			nodeIndex * branchFactor + moveIndex ,False, scores, targetDepth
-			, branchFactor, currentBoardState,moveIndex)
+			      nodeIndex * branchFactor + moveIndex ,False, scores, leafDepth
+			      ,branchFactor, nextBoard,moveIndex)
 			print("End MM\n")
-		moveIndex = 0
-        #find max child value
+
+        #Fint max value in returned scores
 		bestValue = max(moves)
 
-        #print the result of every max node evaluation
+        #print the result
 		print("D"+str(curDepth)+": "+"MAX node no."+str(nodeIndex) +
 			" value: "+ str(bestValue))
 
-		#Print move based on minimax value if we're back at the present turn
-		#this will be the index of the child with best
-		if (curDepth == 0):
-			print("\nYou should MOVE "+str(moves.index(max(moves))+1))
+		#The end: based on the value provide a move
+		if (curDepth == 0): #first recursive call has finished
+			print("So "+playerName + " should now MOVE "
+			+str(moves.index(max(moves))+1))
 
 		#return max child value to parent
 		return bestValue
 
 	else:
-		#for storing the child valuations
+
+		#print node description
+		playerName = "NORTH (MIN)"
+		print(playerName+ " node "+ "with state:")
+		print(currentBoard)
+		print('\n')
+
+		#Child scores will be sent up and stored here
 		moves = [0]*branchFactor
 
-		#Run minimax on each child (future turns)
+		#Run minimax on each child (next turns)
 		for moveIndex in range(0,branchFactor):
-			moves[moveIndex] = minimax(curDepth + 1, nodeIndex * branchFactor + moveIndex
-			,True, scores, targetDepth,branchFactor, currentBoardState,moveIndex)
-		moveIndex = 0
+			print("If "+ playerName + " MOVES "+str(moveIndex+1))
+			print(playerName + " CHILD NO."+str(moveIndex+1)+" MM")
+			print("Its state will be: ")
+
+			#Get board produced by this move from this node
+			nextBoard = makeNextBoard(isMaxTurn, currentBoard, moveIndex)
+			print(str(nextBoard))
+
+			#pass board to child
+			moves[moveIndex] = minimax(curDepth + 1,
+			 				nodeIndex * branchFactor + moveIndex, True, scores,
+							leafDepth,branchFactor, currentBoard,moveIndex)
+
 		#find min child value
 		bestValue = min(moves)
 
-        #print the result of every min node evaluation
+        #print the result
 		print("D"+str(curDepth)+": "+"MIN node no."+str(nodeIndex) +
 			" value: "+ str(bestValue))
 
-		#Print move based on minimax value if we're back at the present turn
-		#this will be the index of the child with best value
-		if (curDepth == 0):
-			print("MOVE "+str(moves.index(min(moves))+1))
+		#The end: based on the value provide a move
+		if (curDepth == 0): #first recurive call end
+			print("So "+playerName + " should now MOVE "+str(moves.index(min(moves))+1))
 
 	    #return min child value to parent
 		return bestValue
 
-def nextBoard(lastTurnWasMax, currentBoardState, moveIndex):
-	curBoardState = currentBoardState.copy()
-	if (lastTurnWasMax):
-		startPit = moveIndex
-		skipScoreOne = False
-		skipScoreTwo = True
-	else:
+def makeNextBoard(fromMaxTurn, currentBoard, moveIndex):
+	curBoard = currentBoard.copy()
+	if (fromMaxTurn):
 		startPit = moveIndex + 8
 		skipScoreOne = True #must skip first pit when sowing
 		skipScoreTwo = False
+	else:
+		startPit = moveIndex
+		skipScoreOne = False
+		skipScoreTwo = True
 	#collect the seeds, emptying the pit
-	seedStash = curBoardState[startPit]
-	curBoardState[startPit] = 0
+	seedStash = curBoard[startPit]
+	curBoard[startPit] = 0
 	curPit = startPit
 	#sow the seeds anti-clockwise
 	while seedStash > 0:
@@ -112,56 +138,64 @@ def nextBoard(lastTurnWasMax, currentBoardState, moveIndex):
 		else:
 			curPit +=1
 		if not((skipScoreOne and curPit == 7) or(skipScoreTwo and curPit == 15) ):
-			curBoardState[curPit]+=1
+			curBoard[curPit]+=1
 			seedStash-=1
-	return curBoardState
+	return curBoard
 
-def evaluateBoard(boardState):
-
+def evaluateBoard(board):
 	#treat pieces on a side as equivalent to being in that side's score
-	seedsOnMaxSide = sum(boardState[8:15])
-	seedsOnMinSide = sum(boardState[0:7])
+	seedsOnMaxSide = sum(board[8:15])
+	seedsOnMinSide = sum(board[0:7])
 
 	score = seedsOnMaxSide - seedsOnMinSide
 	#south - north
 	return score
 
 
-startDepth = 0
-firstLeafIndex = 0
 
-#South is maximising player, set to 1 to run as Norths
-isMaxPlayer = 0
 branchFactor = 7
 
-# Creates a list containing 5 lists, each of 8 items, all set to 0
+#initialize default board state
 columns = 16;
-initialBoardState = [0 for x in range(columns)]
+initialBoard = [0 for x in range(columns)]
 for col in range (0,8):
-	initialBoardState[col] = 7
-	initialBoardState[col+8] = 7
-initialBoardState[7] = 0
-initialBoardState[15] = 0
-# heuristic calculations at leaves
-treeDepth = 1 #2:Agent moves once, trying to minimize the damage opp will make
-              #on its next move
-totalNoOfLeaves = branchFactor ** treeDepth
+	initialBoard[col] = 7
+	initialBoard[col+8] = 7
+initialBoard[7] = 0
+initialBoard[15] = 0
+
+maxTreeDepth = 1
+totalNoOfLeaves = branchFactor ** maxTreeDepth
+
+#Initialize tree leave evaluation holders
 scores = [0]* totalNoOfLeaves
+
+
 moveIndex = 0
 
+#DEBUG: instead of creating heuristics in the tree Leaves
+#I created random ones to test the tree
 #scores = np.random.randint(-98,98, totalNoOfLeaves)
 
 
 #DEBUG:
-treeDepthCheck = math.log(len(scores), branchFactor)
-print("The treeDepth value is : " + str(treeDepthCheck)+"\n")
+maxTreeDepthCheck = math.log(len(scores), branchFactor)
+print("The maxTreeDepth value is : " + str(maxTreeDepthCheck)+"\n")
+
+
+startDepth = 0
+firstIndex = 0
+
+#South is maximising player, set to 1 to run as North
+isMaxPlayer = 1
 
 #minimax must start from top of tree (0)
-#the first leaf that will be reached is the leftmost (0)
-mm_result = minimax(startDepth, firstLeafIndex, isMaxPlayer, scores, treeDepth, branchFactor, initialBoardState,moveIndex)
+#First and only index at D0 is 0
+mmResult = minimax(startDepth, firstIndex, isMaxPlayer, scores, maxTreeDepth
+				,branchFactor, initialBoard, moveIndex)
 
-print("\n"+"The optimal value is : " + str(mm_result)+"\n")
+print("\n"+"The optimal value is : " + str(mmResult)+"\n")
 
 #DEBUG:
-print("EVALUATIONS at depth: "+str(treeDepth)+"\n"+ str(scores))
-print("number of evaluations: "+str(len(scores)))
+print("Leaf scores: "+ str(scores))
+print("Number of evaluations: "+str(len(scores)))
