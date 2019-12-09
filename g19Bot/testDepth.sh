2b6f15247fda4a5bfe108057fd7fbcbc13df25bf
mbaxask2 <sarah.kells@student.manchester.ac.uk>
2019-12-09 17:04:05 +0000

    Adding Mean Scores to Test Depth

--- g19Bot/testDepth.sh
+++ g19Bot/testDepth.sh
@@ -3,9 +3,9 @@ input="TEST.txt"
 rm "TEST.txt"
 printf "%s\t\t%s\t\t%s\t\t%s\n" "DEPTH" "PLAYER" "SPEED" "SCORE" >> TEST.txt
 printf "%s\n" "---------------------------------Game:--------------------------------- "
-for i in {1..25}
+depth=25
+for ((i = 1; i <= depth; i++))
 do
-    printf "\n" >> TEST.txt
     echo "RUNNING PLAYER 1 AT DEPTH $i"
     java -jar ManKalah.jar "python3 -u g19Bot/game.py -d ${i} -m AB" "java -jar MKRefAgent.jar"
     echo "RUNNING PLAYER 2 AT DEPTH $i"
@@ -13,3 +13,23 @@ do
 done 
 printf "%s\n" "-------------------------------Log file:------------------------------- "
 cat TEST.txt
+IFS=$'\n' read -d '' -r -a lines < TEST.txt
+lineNumber=0
+declare -a newList
+
+for x in ${lines[@]} 
+do 
+    #printf "line ${lineNumber}: %s\n" "${x}"
+    let "lineNumber++"
+    newList+=($x)
+done
+let "y=6*depth"
+for ((i = 1; i <= depth; i++))
+do
+    let "x=i*7+(i-1)"
+    #echo "DEPTH \t" ${i}
+    let "score1=${newList[x]}"
+    let "score2=${newList[x+4]}"
+    let "score3=(${score1}+${score2}) / 2 "
+    echo "DEPTH \t" ${i} "MEAN \t" ${score3}
+done
